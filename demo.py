4ddf3139769da5af10f51896f3d1d48c50c9ccc3
chandan499 <chandangoswami854331@gmail.com>
2026-08-19 12:08:00 +0530

    utilsadded

---
+++ demo.py
@@ -0,0 +1,13 @@
+from us_visa.logger import logging
+from us_visa.exception import USvisaException
+logging.info("welcome to custom log")
+
+
+
+import sys
+
+try:
+    a = 2 / 0
+
+except Exception as e:
+    raise USvisaException(e, sys)
